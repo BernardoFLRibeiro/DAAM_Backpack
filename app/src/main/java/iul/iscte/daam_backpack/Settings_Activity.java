228cc656eb20860673ccc812cb00ebc742a92254
BernardoFLRibeiro <38184444+BernardoFLRibeiro@users.noreply.github.com>
2018-05-27 12:24:02 +0100

    ALterada a settings activity

--- app/src/main/java/iul/iscte/daam_backpack/Settings_Activity.java
+++ app/src/main/java/iul/iscte/daam_backpack/Settings_Activity.java
@@ -12,19 +12,61 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Settings_Activity extends MenuPage {
     private DrawerLayout dl;
     private ActionBarDrawerToggle t;
     private NavigationView nv;
     private int position = 0;
+
+    String[] listItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_);
+        fill();
         getSupportActionBar().setTitle("Definições");
         createListen();
         setupDrawer();
     }
+
+    private void fill(){
+
+        listItems = new String[] {
+                "Nome",
+                "Curso",
+                "Universidade",
+                "Password"
+        };
+
+        List<String> settings_list = new ArrayList<String>(Arrays.asList(listItems));
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        ListView  mListView = (ListView) findViewById(R.id.lv_view);
+        mListView.setAdapter(adapter);
+
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the selected item text from ListView
+                String selectedItem = (String) parent.getItemAtPosition(position);
+
+                // Display the selected item text on TextView
+                TextView tv = (TextView)findViewById(R.id.changeTV);
+                tv.setText("Your favorite : " + selectedItem);
+            }
+        });
+    }
+
+
 }
